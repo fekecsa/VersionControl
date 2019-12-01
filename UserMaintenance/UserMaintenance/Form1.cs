aaf674c4a6ce5e7b86adaa8cef2929ef20f1a1e7
Ákos <kos@DESKTOP-KJQKCGS>
2019-12-01 11:02:39 +0100

    IRF2

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.ENTITIES;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<USER> users = new BindingList<USER>();
+
+
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName; // label1
+            label2.Text = Resource1.FirstName; // label2
+            button1.Text = Resource1.Add; // button1
+
+
+            // ...
+
+            // listbox1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new USER()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
